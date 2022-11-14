497ee40cf817a3ff1a559c0695af993191853a73
Kyle Wilson <xlak_@outlook.com>
2022-11-14 20:11:42 +0000

    Update 3 files

--- index.mjs
+++ index.mjs
@@ -1,31 +1,42 @@
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
-import createServer from '@tomphttp/bare-server-node';
-import http from 'http';
-import nodeStatic from 'node-static';
+import createServer from "@tomphttp/bare-server-node";
+import http from "http";
+import nodeStatic from "node-static";
 
-
-const bare =  createServer('/bare/');
-const serve = new nodeStatic.Server('static/');
+const bare = createServer("/bare/");
+const serve = new nodeStatic.Server("static/");
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-		bare.routeRequest(req, res);
-	} else {
-		serve.serve(req, res);
-	}
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    serve.serve(req, res);
+  }
 });
 
-server.on('upgrade', (req, socket, head) => {
-	if (bare.shouldRoute(req, socket, head)) {
-		bare.routeUpgrade(req, socket, head);
-	}else{
-		socket.end();
-	}
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req, socket, head)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
 server.listen({
-	port: process.env.PORT || 8080,
+  port: process.env.PORT || 8081,
+});
+
+const rserve = new nodeStatic.Server("new/");
+
+const rserver = http.createServer();
+
+rserver.on("request", (req, res) => {
+  rserve.serve(req, res);
+});
+
+rserver.listen({
+  port: 8080,
 });
